bd408fccbb31cbad74ca3d2a1b11b366c8fb636b
dhanashree <dhanichavan795@gmail.com>
2023-06-15 19:01:14 +0530

    multiple input

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+  return(
+    <>
+      <h1>My ToDo</h1>
+      <MyTodo/>
+    </>
+  )
+}
+
+function MyTodo(){
+  //s1
+  let [todo, setTodo]= useState({task:"",description:""});
+  let handleTaskAction = (e)=>{
+    //console.log(e.target); => input object
+    let newTodo = {...todo,task:e.target.value};
+    setTodo(newTodo);
+  }
+
+  let addTodoAction = ()=>{
+    //alert(todo.task + todo.decription);
+    console.log(todo);
+  }
+
+  let handleDescryption=(e)=>{
+    //console.log(e.target.value);
+    let newTodo = {...todo,description:e.target.value};
+    setTodo(newTodo);
+
+  }
+
+  return(
+    <>
+      <input className="form-control" type="text" placeholder="Enter todo" value={todo.task} onChange={handleTaskAction}/>
+      <textarea className="form-control" cols="30" rows="3" placeholder="Enter Description" onChange={handleDescryption}></textarea>
+      <input type="button" value="Add Todo" onClick={addTodoAction}/>
+    </>
   );
 }
 
