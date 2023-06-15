f9537d5558ace47a46abe83491deb3f73b11940e
dhanashree <dhanichavan795@gmail.com>
2023-06-16 03:04:33 +0530

    stateful-list

--- src/App.js
+++ src/App.js
@@ -1,48 +1,43 @@
+import {useState} from "react";
 function App(){
   return(
     <>
-    <h1>Props Demo</h1>
-    <ListDemo/>
+      <h1>Stateful Lists</h1>
+      <ListDemo/>
     </>
   );
+  
 
 }
 
 function ListDemo(){
-  let list = ["delhi","mumbai","chennai"];
-  let list1 = [];
-  for(let i=1;i<=10;i++){
-    list.push("");
+  //let list = ["delhi"];
+  //C1: Stateful variable
+  let [list, setList] = useState(["delhi"]);
+
+  //C2: Action Member Function
+  let handleClick = ()=>{
+    //why not push(), cuz it updates same object
+    //setter method requires immutable object
+    //we have to pass new list
+    let newList = [...list,"mumbai"];
+    console.log(newList);
+
+    //DOM::trigger dom
+    setList(newList);
   }
   return(
     <>
-    <h1>Hello world</h1>
-    {/** version: map method is i/o */}
-    {list.map((item) =>  item)};
-    <hr />
-    {/** version 2 */}
-    {list.map((item) => `Hello ${item}` )};
-    <hr />
-    {/** version 3 */}
-    {list.map((item) => (<><h1>Hello {item}</h1> </>))};
-    <hr />
-    {list.map((item)=><h1>Hello</h1>)};
-
+      {/**C3: Event Binding */}
+      <input type="button" value="Add New Item" onClick={handleClick}/>
+      
+      {/**C4: List */}
+      {list.map((item)=>(
+        <h1>Hello {item}</h1>
+      ))}
+      
     </>
-  );
-}
-function MessageDemo({username,city}){
-  //console.log("Props for current component: ",props);
-  //Object {  }
-  //Object { username: "Jae", city: "LA" }
-  return(
-    <>
-    <h1>Hello {username}</h1>
-    <h1>I am from {city}</h1>
-    </>
-  );
+  )
 }
 
 export default App;
-
-
