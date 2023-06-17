7a7796ba434b7ca8005b8e97abecd1842846b3cb
dhanashree <dhanichavan795@gmail.com>
2023-06-17 13:19:11 +0530

    basic-nav-link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import {BrowserRouter,Route, Routes} from "react-router-dom";
+import {BrowserRouter,Link,Route, Routes} from "react-router-dom";
 import MyToDo from "./pages/MyTodo";
 import MyRegistration from "./pages/MyRegistration";
 
@@ -7,11 +7,15 @@ function App(){
   return(
     <>
       <BrowserRouter>
+        <Link to={"/home"}>Home  |</Link>
+        <Link to={"/todo"}>Todo  |</Link>
+        <Link to={"/registration"}>Registration  |</Link>
         <Routes>
           <Route path="/" element={<h1>Hello</h1>}/>
           <Route path="/home" element={<h1>Home</h1>}/>
           <Route path="/todo" element={<MyToDo/>}/>
-          <Route path="/signup" element={<MyRegistration/>}/>
+          <Route path="/registration" element={<MyRegistration/>}/>
+          <Route path="*" element={<h1>Page Not found</h1>}/>
         </Routes>
       </BrowserRouter>
     </>
