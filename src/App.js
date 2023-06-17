a48a75c8b50e877db180d4831719862a0e579a9b
dhanashree <dhanichavan795@gmail.com>
2023-06-17 13:40:53 +0530

    nav-using-button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import {BrowserRouter,Link,Route, Routes} from "react-router-dom";
+import {BrowserRouter,Link,Route, Routes, useNavigate} from "react-router-dom";
 import MyToDo from "./pages/MyTodo";
 import MyRegistration from "./pages/MyRegistration";
 import MyNavLink from "./pages/MyNavLink";
@@ -16,10 +16,32 @@ function App(){
           <Route path="/registration" element={<MyRegistration/>}/>
           <Route path="*" element={<h1>Page Not found</h1>}/>
         </Routes>
+        <MyNavUsingButton/>
       </BrowserRouter>
     </>
     
   );
+}
+
+function MyNavUsingButton(){
+  const navigate = useNavigate();
+  let goToHome=()=>{
+    navigate("/home");
+  }
+  let goToTodo=()=>{
+    navigate("/todo");
+  }
+  let goToRegister=()=>{
+    navigate("/registration");
+  }
+  return(
+    <div>
+      <input type="button" value="Home" onClick={goToHome} />
+      <input type="button" value="Todo" onClick={goToTodo} />
+      <input type="button" value="Registration" onClick={goToRegister} />
+    </div>
+    
+  )
 }
 
 
