49c69016b64ec9bd870548f495a46920683a29b4
dhanashree <dhanichavan795@gmail.com>
2023-06-18 13:06:00 +0530

    registratin-try-catch

--- src/pages/MyRegistration.js
+++ src/pages/MyRegistration.js
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function MyRegistration(){
+
+    let fromRef = useRef();
     let [user, setUser]= useState({
       username:"", 
       password:"", 
@@ -26,28 +28,46 @@ function MyRegistration(){
     }
   
     let registerAction = async()=>{
-      //backend
-      let url= `http://localhost:4000/adduser?username=${user.username}&password=${user.password}&email=${user.email}&mobile=${user.mobile}`
-      await fetch(url);
-  
-      let newuser={
-        username:"", 
-        password:"", 
-        email:"",
-        mobile:""
-      };
-  
-      setUser(newuser);
+      try{
+          //form-validation
+        fromRef.current.classList.add("was-validated");
+        let formStatus = fromRef.current.checkValidity();
+        if(!formStatus){
+          return;
+        }
+        //backend
+        let url= `http://localhost:4000/adduser?username=${user.username}&password=${user.password}&email=${user.email}&mobile=${user.mobile}`
+        let res = await fetch(url);
+        if(res.status !=200){
+          let serverMsg = await res.text();
+          throw new Error(serverMsg);
+        }
+    
+        let newuser={
+          username:"", 
+          password:"", 
+          email:"",
+          mobile:""
+        };
+    
+        setUser(newuser);
+        fromRef.current.classList.remove("was-validated");
+      }catch(err){
+        alert(err.message);
+      }
+      
     }
   
     return(
       <>
+        <form ref={fromRef} className="needs-validation">
         <input 
           className="form-control" 
           type="text" 
           placeholder="Enter username" 
           value={user.username} 
           onChange={handleNameChange}
+          required
         />
         <input 
           className="form-control" 
@@ -55,6 +75,7 @@ function MyRegistration(){
           placeholder="Enter password" 
           value={user.password} 
           onChange={handlePassChange}
+          required
         />
         <input 
           className="form-control" 
@@ -62,6 +83,7 @@ function MyRegistration(){
           placeholder="Enter email" 
           value={user.email} 
           onChange={handleEmailChange}
+          required
         />
         <input 
           className="form-control" 
@@ -69,6 +91,7 @@ function MyRegistration(){
           placeholder="Enter mobile" 
           value={user.mobile}
           onChange={handleMobileChange}
+          required
         />
         <input 
           className="w-100" 
@@ -76,6 +99,7 @@ function MyRegistration(){
           value="Register"
           onClick={registerAction} 
         />
+        </form>
       </>
     )
   }
